1fc07e3f3b252bec2d23d1d684cf3180b92b96f4
Xavier <xavier@x-gsm.fr>
2018-01-29 20:59:58 +0100

    dirmngr

--- filebot/filebot.sh
+++ filebot/filebot.sh
@@ -18,6 +18,7 @@ if [ $? -eq 1 ]; then
 	exit 1
 else
 		if [ ! -f "/etc/apt/sources.list.d/webupd8team-java.list" ];then
+		aptitude install dirmngr
 		echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 		echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 		apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
